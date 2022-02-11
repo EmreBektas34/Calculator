65ca30294bcacec1dd7bed5d26db8529f328bd7d
emr.bektas34 <emr.bektas34@gmail.com>
2022-02-11 15:10:19 +0300

    Proje dosyası ekle.

---
+++ Calculator/Calculator.iOS/CustomRenderer/CustomEntryRenderer.cs
@@ -0,0 +1,26 @@
+﻿using Calculator.CustomRenderers;
+using Calculator.iOS.CustomRenderer;
+using Foundation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryRenderer))]
+namespace Calculator.iOS.CustomRenderer
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            Control.Layer.BorderWidth = 0;
+            Control.BorderStyle = UITextBorderStyle.None;
+        }
+    }
+}
